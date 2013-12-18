d44d1461b43dcf53f1697d94de3e822418ada137
Aksel Gresvig <aksel@agresvig.com>
2013-12-18 21:42:26 +0100

    Updating Travis build procedure. Thanks @wesleyhales

--- build/gh-pages-report.sh
+++ build/gh-pages-report.sh
@@ -1,19 +1,17 @@
 export REPO="$(pwd | sed s,^/home/travis/build/,,g)"
 echo -e "Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
  
-#Set git user
-git config --global user.email "aksel@agresvig.com"
-git config --global user.name "Travis"
- 
-#Set upstream remote
-git remote add upstream https://${GH_TOKEN}@github.com/AGresvig/blog.akselgresvig.com.git > /dev/null
- 
-git fetch -qn upstream > /dev/null
- 
+TEST_PAGE="http://agresvig.github.io/blog.akselgresvig.com/"
+LATEST_SHA=$(git rev-parse HEAD)
+
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    git checkout gh-pages
-    phantomjs loadreport.js http://agresvig.github.io/blog.akselgresvig.com/ performance json
+git checkout gh-pages
+    ps -eo pcpu,pid,user,args
+    sleep 1m
+    phantomjs loadreport.js ${TEST_PAGE} performance json $LATEST_SHA
+    phantomjs speedreport.js ${TEST_PAGE} $LATEST_SHA
     git add -f reports/.
+    git add -f speedreports/.
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
     git push https://${GH_TOKEN}@github.com/${REPO} gh-pages > /dev/null
 fi
