16eb63282f6ef149ac58815e7484e84c87997f0d
Aksel Gresvig <aksel@agresvig.com>
2013-12-19 09:53:28 +0100

    More debug-statements to gh-report script

--- build/gh-pages-report.sh
+++ build/gh-pages-report.sh
@@ -1,17 +1,18 @@
 export REPO="$(pwd | sed s,^/home/travis/build/,,g)"
-echo -e "Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
+echo -e "*** Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
  
 TEST_PAGE="http://agresvig.github.io/blog.akselgresvig.com/"
 LATEST_SHA=$(git rev-parse HEAD)
 
-echo "STATUS"
+echo "*** STATUS"
 git status
 git branch -a
-ls -al
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    git fetch origin gh-pages
-    git checkout gh-pages
+    #git fetch origin gh-pages
+    #git checkout gh-pages
+    echo "*** ls -al src"
+    ls -al src
     ps -eo pcpu,pid,user,args
     sleep 1m
     phantomjs src/loadreport.js ${TEST_PAGE} performance json $LATEST_SHA
