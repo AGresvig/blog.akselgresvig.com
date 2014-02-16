38488d4284230b14abe34e45120bb3b4c815f317
Aksel Gresvig <aksel@agresvig.com>
2014-02-16 15:57:36 -0800

    Adding sleep to post build script

--- travis/create_loadreports.sh
+++ travis/create_loadreports.sh
@@ -1,18 +1,21 @@
 TEST_PAGE="http://blog.akselgresvig.com/"
 LATEST_SHA=$(git rev-parse HEAD)
 
-echo -e "*** Running load- and speed-reports on $TEST_PAGE for revision $LATEST_SHA"
-
 git branch -a
 git checkout master
 git pull
 
+echo -e "*** Running load- and speed-reports on $TEST_PAGE for revision $LATEST_SHA"
 #Run report scripts using PhantomJS
 phantomjs travis/loadreport/loadreport.js ${TEST_PAGE} filmstrip $LATEST_SHA
 phantomjs travis/loadreport/speedreport.js ${TEST_PAGE} $LATEST_SHA
 
+echo -e "*** Generating reports/index.html"
+
 #Create index.html of reports in report-dir
 ./travis/generate-index.sh reports > reports/index.html
+
+sleep 10s
 
 #Add the resulting reports to the gh-branch (so its available on site)
 git checkout gh-pages
