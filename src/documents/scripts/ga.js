7036c5cbf007d75d037d2f218f9bf7acabd057ad
Aksel Gresvig <aksel@agresvig.com>
2014-03-04 22:18:13 -0800

    Added text plugin for on-demand markdown rendering. Corrected some GA code

--- src/documents/scripts/ga.js
+++ src/documents/scripts/ga.js
@@ -1,8 +1,8 @@
-var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+var _gaq=[['_setAccount','UA-39146035-2'],['_trackPageview']];
 (function(d,t){
   var g=d.createElement(t),
       s=d.getElementsByTagName(t)[0];
 
-  g.src='//www.google-analytics.com/ga.js';
+  g.src='//www.google-analytics.com/analytics.js';
   s.parentNode.insertBefore(g,s)}(document,'script')
 );
